2b1abfa96d800f964524320f6bf672cfa7427ff5
rover0811 <rover0811@hotmail.com>
2025-05-04 23:33:57 +0900

    [Refactor] WhisperX 관련 리소스 제거 및 Spring App 인프라 구성 추가
    WhisperX 관련 GCP 리소스를 제거하고, Spring App 배포를 위한 Compute Engine 및 방화벽 설정을 추가했습니다. 이를 통해 Spring App이 프리티어 기반 환경에서 실행될 수 있도록 구성했습니다.

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "audio_bucket_name" {
-  description = "오디오 파일 저장용 버킷 이름"
-  value       = google_storage_bucket.audio_bucket.name
-}
-
-output "transcript_bucket_name" {
-  description = "트랜스크립션 결과 저장
-}
